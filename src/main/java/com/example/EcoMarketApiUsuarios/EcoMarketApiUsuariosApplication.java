3cdff3b6ce9a6e9fa6d60c6356813dd9eeed5b36
ALONSODIAZ <ALO.DIAZ@DUOCUC.CL>
2026-01-12 21:01:31 -0300

    Init EcoMarketApiUsuarios

---
+++ src/main/java/com/example/EcoMarketApiUsuarios/EcoMarketApiUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcoMarketApiUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMarketApiUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoMarketApiUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EcoMarketApiUsuarios/EcoMarketApiUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcoMarketApiUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMarketApiUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0ddfbc0cdf2241ec7590431df0ee9573e58fbdd8
Constanza Rojas <cons.rojasb@duocuc.cl>
2026-01-13 15:42:29 -0300

    cony

---
+++ EcomarketAPIinventario/EcomarketAPI/src/main/java/com/example/EcomarketAPI/EcomarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcomarketAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomarketApiApplication.class, args);
+	}
+
+}
---
+++ EcomarketAPIinventario/EcomarketAPI/src/test/java/com/example/EcomarketAPI/EcomarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcomarketAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

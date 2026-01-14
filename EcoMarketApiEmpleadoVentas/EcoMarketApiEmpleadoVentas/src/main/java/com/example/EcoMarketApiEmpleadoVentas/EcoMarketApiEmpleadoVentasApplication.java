b3b135be9c5f5991f931074e1d62f118b73c35c3
Tiare-Gomez <tia.gomez@duocuc.cl>
2026-01-14 16:08:15 -0300

    Creacion microservicio para gestion de ventas

---
+++ EcoMarketApiEmpleadoVentas/EcoMarketApiEmpleadoVentas/src/main/java/com/example/EcoMarketApiEmpleadoVentas/EcoMarketApiEmpleadoVentasApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcoMarketApiEmpleadoVentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMarketApiEmpleadoVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoMarketApiEmpleadoVentasApplication.class, args);
+	}
+
+}

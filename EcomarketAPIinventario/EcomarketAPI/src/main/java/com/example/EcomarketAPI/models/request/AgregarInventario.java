441aff88b97aaea9d68eecf8a6ed567718243cf8
Constanza Rojas <cons.rojasb@duocuc.cl>
2026-01-15 22:46:41 -0300

    pruebas de Inventario

--- EcomarketAPIinventario/EcomarketAPI/src/main/java/com/example/EcomarketAPI/models/request/AgregarInventario.java
+++ EcomarketAPIinventario/EcomarketAPI/src/main/java/com/example/EcomarketAPI/models/request/AgregarInventario.java
@@ -1,6 +1,8 @@
 package com.example.EcomarketAPI.models.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
@@ -8,13 +10,15 @@ public class AgregarInventario {
     @NotBlank
     private String nombreProducto;
 
-    @NotBlank
+    @NotNull
+    @Positive
     private int cantidadStock;
 
     @NotBlank
     private String categoria;
     
-    @NotBlank
+    @NotNull
+    @Positive
     private double precio;
 
     

f6db1f9b4d6884237ef6b25b404fe6954d660b35
ALONSODIAZ <ALO.DIAZ@DUOCUC.CL>
2026-01-21 18:30:44 -0300

    Integracion de hateoas

--- EcoMarketApiUsuarios/src/main/java/com/example/EcoMarketApiUsuarios/models/entities/User.java
+++ EcoMarketApiUsuarios/src/main/java/com/example/EcoMarketApiUsuarios/models/entities/User.java
@@ -1,5 +1,7 @@
 package com.example.EcoMarketApiUsuarios.models.entities;
 
+import org.springframework.hateoas.RepresentationModel;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,7 +13,7 @@ import lombok.Data;
 @Entity
 @Data
 @Table(name = "users")
-public class User {
+public class User extends RepresentationModel<User> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

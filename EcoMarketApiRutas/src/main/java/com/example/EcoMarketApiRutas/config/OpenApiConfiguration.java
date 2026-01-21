6980a6768f6f7446492d5b65e52b374a38123dbe
ALONSODIAZ <ALO.DIAZ@DUOCUC.CL>
2026-01-20 21:59:41 -0300

    Integracion de Swagger

---
+++ EcoMarketApiRutas/src/main/java/com/example/EcoMarketApiRutas/config/OpenApiConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.EcoMarketApiRutas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI documentacionOpenApi() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Gestion de Rutas")
+                .version("1.0")
+                .description("Gestiona rutas")
+                .contact(new Contact()
+                    .name("Orcaleritos")));
+    }
+}

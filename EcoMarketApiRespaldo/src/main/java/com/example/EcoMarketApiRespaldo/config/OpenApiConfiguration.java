285f6e7fa49e66ae0f7d9c2d0ced37225f0d1040
ALONSODIAZ <ALO.DIAZ@DUOCUC.CL>
2026-01-20 21:53:07 -0300

    Integracion de Swagger

---
+++ EcoMarketApiRespaldo/src/main/java/com/example/EcoMarketApiRespaldo/config/OpenApiConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.EcoMarketApiRespaldo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI documentacionOpenApi() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Gestion de Respaldos")
+                .version("1.0")
+                .description("Gestiona respaldos del sistema")
+                .contact(new Contact()
+                    .name("Orcaleritos")));
+    }
+}
---
+++ EcoMarketApiRespaldo/src/main/java/com/example/EcoMarketApiRespaldo/models/dto/VersionInfo.java
@@ -0,0 +1,3 @@
+package com.example.EcoMarketApiRespaldo.models.dto;
+
+public record VersionInfo(String nombreApp, String version) {}

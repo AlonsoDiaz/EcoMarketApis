203b6b8759c7e88c22fe7d9b0e3f6ca9d2dc07fc
Tiare-Gomez <tia.gomez@duocuc.cl>
2026-01-23 11:04:22 -0300

    pruebas unitarias

---
+++ EcoMarketApiEmpleadoFacturacion/src/test/java/com/example/EcoMarketApiEmpleadoFacturacion/EcoMarketApiEmpleadoFacturacionApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.EcoMarketApiEmpleadoFacturacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EcoMarketApiEmpleadoFacturacionApplicationTests {
+    @Test
+	void contextLoads() {
+	}
+
+}

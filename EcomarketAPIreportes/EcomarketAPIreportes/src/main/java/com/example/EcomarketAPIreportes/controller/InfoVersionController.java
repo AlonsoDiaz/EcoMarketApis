c88c6d9ea5bfcf80cd38814c882b45ebbbfdadd1
Constanza Rojas <cons.rojasb@duocuc.cl>
2026-01-25 00:18:42 -0300

    aws reportes

--- EcomarketAPIreportes/EcomarketAPIreportes/src/main/java/com/example/EcomarketAPIreportes/controller/InfoVersionController.java
+++ EcomarketAPIreportes/EcomarketAPIreportes/src/main/java/com/example/EcomarketAPIreportes/controller/InfoVersionController.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@RequestMapping()
+@RequestMapping("/api/version")
 @RestController
 public class InfoVersionController {
 
